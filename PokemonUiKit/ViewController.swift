b0d7005ee0177ce518c9777a1fae1f0665fac6a8
Alexandr Rodionov <alexrodionby@gmail.com>
2022-09-23 11:12:43 +0300

    init(app) start new project, first init off app

---
+++ PokemonUiKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonUiKit
+//
+//  Created by Alexandr Rodionov on 23.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
